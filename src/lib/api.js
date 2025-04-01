a411403c2eb1b21e541a2c318f080aa985962342
PiyushKhewalkar <work.piyushkkr@gmail.com>
2025-04-01 13:15:37 +0530

    updated api endpoints

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5500"
+const API_URL = "https://ai-copywriter-backend.onrender.com"
 
 export const fetchAllPersona = async() => {
     try {
