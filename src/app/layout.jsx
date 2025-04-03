348cf45edeb6f29a3ebd181106cc033b08bacdf1
PiyushKhewalkar <work.piyushkkr@gmail.com>
2025-04-03 20:35:58 +0530

    minor

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { SidebarProvider} from "@/components/ui/sidebar.jsx"
 import { AppSidebar } from "@/components/layout/Sidebar.jsx"
-import Header from "@/components/layout/header.jsx"
+import Header from "@/components/layout/Header.jsx"
  
 export default function Layout({ children }) {
   return (
